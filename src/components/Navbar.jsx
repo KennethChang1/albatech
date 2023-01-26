803e08779dd48eafcbb1610c439c1dfb8f4f1514
Kenneth Chang <kenneth.kristopher27@gmail.com>
2023-01-26 16:06:09 +0700

    add fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,24 @@
 import { Link } from "react-router-dom";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
   return (
-    <nav className="nav">
-      <img src="/alba-logo.svg" alt="" />
-      <div className="nav-links">
-        <Link to="/services">Service</Link>
-        <Link to="/services">About</Link>
-        <Link to="/services">Portoflio</Link>
-        <Link to="/services">Blog</Link>
-        <Link to="/services">Career</Link>
-      </div>
-      <button className="btn">Contact Us</button>
-    </nav>
+    <div className="nav-container">
+      <nav className="nav">
+        <img className="logo" src="/alba-logo.svg" alt="" />
+        <div className={`nav-links ${nav ? "" : "hideNav"}`}>
+          <Link to="/services">Service</Link>
+          <Link to="/services">About</Link>
+          <Link to="/services">Portoflio</Link>
+          <Link to="/services">Blog</Link>
+          <Link to="/services">Career</Link>
+        </div>
+        <button className="btn">Contact Us</button>
+        <img className="nav-menu" src="/icons8-hamburger-menu-24.png" alt="" onClick={()=>setNav(prev => !prev)}/>
+      </nav>
+    </div>
+
   );
 };
 
